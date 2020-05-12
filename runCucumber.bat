66e6d62e04d17f1f1d006598b76e21b180e1961d
Damm999 <reymysterio390@gmail.com>
2020-05-12 11:02:19 +0530

    1st commit

--- runCucumber.bat
+++
@@ -1,2 +0,0 @@
-cd C:\eclipe_workspace\cucumber_workspace\CucumberFramework
-mvn clean compile test
--- src/main/java/Constants/GlobalConstants.java
+++
@@ -1,6 +0,0 @@
-package Constants;
-
-public class GlobalConstants {
-
-	public static final String Config_Properties_Loc = "./src/main/java/resources/config.properties";
-}
